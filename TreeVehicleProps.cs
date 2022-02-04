ec554dd2110842dd15b9b249040ee68851106cec
sway2020 <65274619+sway2020@users.noreply.github.com>
2022-02-04 13:51:58 -0800

    v2.0.2

--- TreeVehicleProps.cs
+++ TreeVehicleProps.cs
@@ -14,7 +14,7 @@ namespace TreeVehicleProps
 {
     public class Mod : IUserMod
     {
-        public const string version = "2.0.1";
+        public const string version = "2.0.2";
         public string Name => "Tree & Vehicle Props " + version;
         public string Description
         {
